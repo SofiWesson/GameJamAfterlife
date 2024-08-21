bfe22fcfbc40bfe653228fb8fb61ce107250fa43
Sofi <sofiwesson@gmail.com>
2024-08-21 19:17:10 +1000

    camera zoom

--- gamejamafterlife/Scripts/CameraController.gd
+++ gamejamafterlife/Scripts/CameraController.gd
@@ -11,21 +11,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
-	#Input.is_action_just_released("ZoomIn"):
-		#camera.position.z -= transform.basis.z * zoomSpeed * delta
-	#if Input.is_action_pressed("ZoomOut"):
-	#	camPos.z -= zoomSpeed * delta
-	#position = to_global(pos)
 
 func _physics_process(delta: float) -> void:
+	CameraMovement(delta)
+	CameraZoom(delta)
+
+func CameraMovement(delta: float):
 	var moveDirection := Vector3.ZERO
+	# which direction on both axis to move along
 	moveDirection.x = Input.get_action_strength("MoveRight") - Input.get_action_strength("MoveLeft")
 	moveDirection.z = Input.get_action_strength("MoveBackward") - Input.get_action_strength("MoveForward")
-	if Input.is_action_pressed("MoveForward"):
-		position -= transform.basis.z.normalized() * speed * delta
-	if Input.is_action_pressed("MoveBackward"):
-		position += transform.basis.z.normalized() * speed * delta
-	if Input.is_action_pressed("MoveRight"):
-		position += transform.basis.x.normalized() * speed * delta
-	if Input.is_action_pressed("MoveLeft"):
-		position -= transform.basis.x.normalized() * speed * delta
+	# move in direction in local space
+	position += moveDirection.z * transform.basis.z.normalized() * speed * delta
+	position += moveDirection.x * transform.basis.x.normalized() * speed * delta
+
+func CameraZoom(delta: float):
+	# zooms camera in and out when scrolling
+	if Input.is_action_just_pressed("ZoomIn"):
+		camera.position -= camera.transform.basis.z.normalized() * zoomSpeed * delta
+	if Input.is_action_just_pressed("ZoomOut"):
+		camera.position += camera.transform.basis.z.normalized() * zoomSpeed * delta

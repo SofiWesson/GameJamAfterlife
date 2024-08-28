fe6e8b22c0d9a507c8e94ec4ff0dd47e2c5225d2
Sofi <sofiwesson@gmail.com>
2024-08-28 11:56:28 +1000

    state machine set up

---
+++ gamejamafterlife/Scripts/PlayState.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func Update():
+	print("Play State Active")
---
+++ gamejamafterlife/Scripts/StateMachine.gd
@@ -0,0 +1,37 @@
+extends Node
+
+@onready var playState: Node = $PlayState
+@onready var buildState: Node = $BuildState
+
+var isPlayState: bool = true
+
+var stateStack: Array
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	stateStack.push_back(playState)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ChangeState"):
+		if stateStack[0] == playState:
+			ChangeToBuildMode()
+		else:
+			ChangeToPlayMode()
+
+	stateStack[0].Update()
+
+func ChangeToPlayMode():
+	stateStack.push_back(playState)
+	ChangeMode()
+	print("play mode")
+
+func ChangeToBuildMode():
+	stateStack.push_back(buildState)
+	ChangeMode()
+	print("build mode")
+
+func ChangeMode():
+	stateStack[0].process_mode = Node.PROCESS_MODE_DISABLED
+	stateStack.back().process_mode = Node.PROCESS_MODE_PAUSABLE
+	stateStack.pop_front()
